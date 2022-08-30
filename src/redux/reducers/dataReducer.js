5383d96cc9894679576c62d4cced0f2e6bb8d781
tribagus <tri.bagus21@gmail.com>
2022-08-30 19:51:51 +0700

    add redux

---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,13 @@
+import TYPES from "../types"
+
+const initState = {
+  loading: false,
+  data: [],
+  error: "",
+}
+
+const dataReducer = (state = initState, action) => {
+  return state
+}
+
+export default dataReducer
